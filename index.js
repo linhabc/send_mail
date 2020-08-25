a5c6aa5c9262e8b19efdb823e54a651ad74c73d7
linh nguyen <jonathanrocrach2@gmail.com>
2020-08-25 13:50:01 +0700

    fix

--- index.js
+++ index.js
@@ -35,26 +35,26 @@ var _getAllFilesFromFolder = async function (dir, folderIndex) {
         index + " " + arr.length + " " + folderIndex + " " + folders.length
       );
 
-      if (index == arr.length - 1 && folderIndex == folders.length - 1) {
-        console.log("Sending mail with total: " + total);
-        var options = {
-          uri: "http://10.4.200.20:5005/api/v1/send-mail",
-          method: "POST",
-          json: {
-            // send_to: "hau.nguyentat@mobifone.vn",
-            send_to: "jonathanrocrach2@gmail.com",
-            cc: "jonathanrocrach2@gmail.com",
-            subject: "[Cảnh báo hệ thống]",
-            content: "Total crawled phone numbers: " + total,
-          },
-        };
-        request(options, function (error, response, body) {
-          if (!error && response.statusCode == 200) {
-            console.log(body);
-          }
-          if (error) console.log("error:" + error);
-        });
-      }
+      // if (index == arr.length - 1 && folderIndex == folders.length - 1) {
+      //   console.log("Sending mail with total: " + total);
+      //   var options = {
+      //     uri: "http://10.4.200.20:5005/api/v1/send-mail",
+      //     method: "POST",
+      //     json: {
+      //       // send_to: "hau.nguyentat@mobifone.vn",
+      //       send_to: "jonathanrocrach2@gmail.com",
+      //       cc: "jonathanrocrach2@gmail.com",
+      //       subject: "[Cảnh báo hệ thống]",
+      //       content: "Total crawled phone numbers: " + total,
+      //     },
+      //   };
+      //   request(options, function (error, response, body) {
+      //     if (!error && response.statusCode == 200) {
+      //       console.log(body);
+      //     }
+      //     if (error) console.log("error:" + error);
+      //   });
+      // }
     });
 
     if (stat && stat.isDirectory()) {
@@ -65,10 +65,30 @@ var _getAllFilesFromFolder = async function (dir, folderIndex) {
   return results;
 };
 
-// setTimeout(() => {
-//   // = total;
-//   console.log("total: " + process.env["L_TOTAL"]);
-// }, 3000);
+setTimeout(() => {
+  // = total;
+
+  console.log("Sending mail with total: " + total);
+  var options = {
+    uri: "http://10.4.200.20:5005/api/v1/send-mail",
+    method: "POST",
+    json: {
+      // send_to: "hau.nguyentat@mobifone.vn",
+      send_to: "jonathanrocrach2@gmail.com",
+      cc: "jonathanrocrach2@gmail.com",
+      subject: "[Cảnh báo hệ thống]",
+      content: "Total crawled phone numbers: " + total,
+    },
+  };
+  request(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      console.log(body);
+    }
+    if (error) console.log("error:" + error);
+  });
+
+  console.log("total: " + total);
+}, 10000);
 
 folders.forEach((fileName, folderIndex) => {
   _getAllFilesFromFolder(fileName, folderIndex);
